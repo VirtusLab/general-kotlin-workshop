08a04ccc5143163d455a7e912771886cb938769f
Paweł Marks <pmarks@virtuslab.com>
2019-10-05 23:15:03 +0200

    Renaming so examples now compile

--- instructor/advanced/org/jetbrains/kotlinworkshop/advanced/_5Reified/_6RestrictionsOnReifiedTypeParameters.kt
+++ instructor/advanced/org/jetbrains/kotlinworkshop/advanced/_5Reified/_6RestrictionsOnReifiedTypeParameters.kt
@@ -2,10 +2,10 @@ package org.jetbrains.kotlinworkshop.advanced._5Reified
 
 // Which of the following calls won't compile?
 
-//inline fun <reified T> foo(t: T) {
+inline fun <reified T> baz(t: T) {
 //    bar(t)
-//}
+}
 
 fun <T> bar(t: T) {
-//    foo(t)
+//    baz(t)
 }
